8efe683de30f98e377c67ef0840bc532ef54522d
TheGauravAdhikari <adhikarigaurav87@gmail.com>
2025-04-10 17:26:50 +0545

    Add project files.

---
+++ crud/Controllers/StudentsController.cs
@@ -0,0 +1,81 @@
+﻿using crud.Data;
+using crud.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Student viewmodel)
+        {
+            var student = new Student
+            {
+                Email = viewmodel.Email,
+                Name = viewmodel.Name,
+                Phone = viewmodel.Phone,
+                IsActive = viewmodel.IsActive
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+            return View(students);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                student.Email = viewModel.Email;
+                student.Name = viewModel.Name;
+                student.Phone = viewModel.Phone;
+                student.IsActive = viewModel.IsActive;
+
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+            if (student is not null)
+            {
+                dbContext.Students.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
+
+    }
+}
---
+++ crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using crud.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
